5af2d3685f4e5cd293961b4f68b0737746be151a
Damian Sire <damiansire@gmail.com>
2024-06-08 15:26:13 -0300

    feat: generate base structure

---
+++ src/app/features/finances/components/date-handler/date-handler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-handler',
+  standalone: true,
+  imports: [],
+  templateUrl: './date-handler.component.html',
+  styleUrl: './date-handler.component.sass'
+})
+export class DateHandlerComponent {
+
+}
