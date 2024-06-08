f3b1488b96ea4d24ff8d77815070af736fe572dd
Damian Sire <damiansire@gmail.com>
2024-06-08 15:29:43 -0300

    feat: add transactions page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { FinancesComponent } from './features/finances/pages/finances.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: FinancesComponent }];
