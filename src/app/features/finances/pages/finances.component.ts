9563fbfe5bc339da1dc730e26f5a3edb38146ae9
Damian Sire <damiansire@gmail.com>
2024-06-15 14:43:22 -0300

    feat: add tabs to finances page

--- src/app/features/finances/pages/finances.component.ts
+++ src/app/features/finances/pages/finances.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TransactionsPageComponent } from './transactions-page/transactions-page.component';
+import { StatisticsPageComponent } from './statistics-page/statistics-page.component';
+import { ExchangesPageComponent } from './exchanges-page/exchanges-page.component';
 
 @Component({
   selector: 'app-finances',
   standalone: true,
-  imports: [],
   templateUrl: './finances.component.html',
-  styleUrl: './finances.component.sass'
+  styleUrl: './finances.component.sass',
+  imports: [
+    MatTabsModule,
+    TransactionsPageComponent,
+    StatisticsPageComponent,
+    ExchangesPageComponent,
+  ],
 })
-export class FinancesComponent {
-
-}
+export class FinancesComponent {}
