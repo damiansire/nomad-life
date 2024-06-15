450e32b786a13ebb13c33c8cc98b785d70a4ba68
Damian Sire <damiansire@gmail.com>
2024-06-15 14:37:04 -0300

    build: add angular material with animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,6 +21,6 @@ export const appConfig: ApplicationConfig = {
     }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          }),
+          }), provideAnimationsAsync(),
   ],
 };
